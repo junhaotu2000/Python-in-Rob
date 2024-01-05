4561d27b2f1f15aeb4dfd5c5ca88df7b39bad687
Aaron <junhaotu@umich.edu>
2024-01-05 14:10:10 -0500

    Update

--- hat.py
+++
@@ -1,9 +0,0 @@
-import random
-
-class Hat:
-    def __init__(self):
-        self.houses = ["Gryffindor", "Hullepuff", "Ravenclaw", "Slytherin"]       
-        
-    def sort(self, name):
-        print(name, "is in", random.choice(self.houses))
-        
