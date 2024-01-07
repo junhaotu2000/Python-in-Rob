12a99de540133c272cf1d877736992e0269c4126
Aaron <junhaotu@umich.edu>
2024-01-06 19:46:25 -0500

    All L8

--- L8. OOP/vault.py
+++ L8. OOP/vault.py
@@ -1,28 +1,3 @@
-class Vault:
-    def __init__(self, galleons = 0, sickles = 0, kunts = 0) -> None:
-        self.galleons = galleons
-        self.sickles = sickles
-        self.kunts = kunts
-    
-    def __str__(self):
-        return f"{self.galleons}, {self.sickles}, {self.kunts}"
-    
-    def __add__(self, other):
-        galleons = self.gallens + other.galleons
-        sickles = self.sickles + other.sickles
-        kunts = self.kunts + other.kunts
-        return Vault(galleons, sickles, kunts)
-                
-potter = Vault(100, 50, 25)
-print(potter)
-
-weasley = Vault(25, 50, 100)
-print(weasley)
-
-# direct add - TypeError: unsupported operand type(s) for +: 'Vault' and 'Vault'
-# overloading add to resolve this issue
-total = potter + weasley
-
 
 
 
