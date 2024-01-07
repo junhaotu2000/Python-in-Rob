da40721c31fc4a5bcf28b10d7e6599bd3b78ba24
Aaron <junhaotu@umich.edu>
2024-01-07 17:43:27 -0500

    FINISH CS50!!!

--- L9. Et Cetera/test.py
+++ L9. Et Cetera/test.py
@@ -1,16 +1,8 @@
-# argparse lib - simpify I/O process
-import argparse
+import numpy as np
 
-# Create the parser and add a description
-parser = argparse.ArgumentParser(description="Meow like a cat")
 
-# Add arguments
-parser.add_argument("-n", default=1, help="number of times to meow", type=int)
-parser.add_argument("-f", default=1, help="volume of meow", type=int)
+x = np.random.rand(10000, 10000)
+y = np.random.rand(10000, 10000)
+z = x @ y
 
-# Parse arguments
-args = parser.parse_args()
-
-# Print "meow" with the specified frequency and volume
-for _ in range(args.n):
-    print("meow " * args.f)
+print(z)
